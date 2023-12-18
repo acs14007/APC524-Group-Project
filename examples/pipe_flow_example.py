1aebc033812d86540bd68db8480d0a869fbc763a
Aaron Spaulding <15004857+acs14007@users.noreply.github.com>
2023-12-17 23:39:39 -0500

    Added the pipe flow example image

--- examples/pipe_flow_example.py
+++ examples/pipe_flow_example.py
@@ -10,5 +10,7 @@ a = Environment(F=(1.0, 0.0), dt=0.001, boundary_conditions=boundary_conditions)
 
 a.run_many_steps(5000)
 
-# a.plot_quiver_plot()
-a.plot_streamline_plot()
+a.plot_quiver_plot(filepath="../Figures/pipe_flow_example.png",
+                   number_of_items_to_skip=3,
+                   title="")
+# a.plot_streamline_plot()
