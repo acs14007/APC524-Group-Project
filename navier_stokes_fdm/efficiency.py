ca0a5b3c4a3a29a97fc7a7c4ae49e3264d223c4a
Aaron Spaulding <15004857+acs14007@users.noreply.github.com>
2023-12-17 22:40:15 -0500

    Updated formating to match PEP8 E501

--- navier_stokes_fdm/efficiency.py
+++ navier_stokes_fdm/efficiency.py
@@ -1,16 +1,20 @@
 """
 Fluid Flow Simulation Script
 
-This Python script performs fluid flow simulations using the Navier-Stokes equations. It sets up an environment with defined parameters, boundary conditions, and objects. The core simulation logic is encapsulated in the `run_simulation` function.
+This Python script performs fluid flow simulations using the Navier-Stokes equations.
+It sets up an environment with defined parameters, boundary conditions, and objects.
+The core simulation logic is encapsulated in the `run_simulation` function.
 
 Key Components:
-- Imports necessary modules and classes.
-- `run_simulation` initializes the environment, applies boundary conditions, introduces objects, and runs the simulation.
-- The main section profiles the simulation's performance using cProfile and saves data to 'simulation_profile.prof'.
+- Imports the necessary modules and classes.
+- `run_simulation` initializes the environment, applies boundary conditions, introduces objects,
+    and runs the simulation.
+- The main section profiles the simulation's performance using cProfile and saves data to
+    'simulation_profile.prof'.
 
-This script is part of a broader fluid dynamics simulation project, facilitating simulations, profiling, and optimization.
+This script is part of a broader fluid dynamics simulation project, facilitating simulations, profiling,
+    and optimization.
 """
-
 
 import cProfile
 from navier_stokes_fdm.environment import Environment
@@ -22,9 +26,10 @@ from navier_stokes_fdm.boundary_condition import (
     TopSideFixedVelocityBoundaryCondition,
     BottomSideFixedVelocityBoundaryCondition,
     LeftSidePeriodicBoundaryCondition,
-    RightSidePeriodicBoundaryCondition
+    RightSidePeriodicBoundaryCondition,
 )
 from navier_stokes_fdm.object import Rectangle
+
 
 def run_simulation():
     # Initialize the environment
@@ -33,10 +38,18 @@ def run_simulation():
     # Add boundary conditions
     env.boundary_conditions.append(TopSideNoSlipBoundaryCondition())
     env.boundary_conditions.append(BottomSideNoSlipBoundaryCondition())
-    env.boundary_conditions.append(LeftSideFixedVelocityBoundaryCondition(u_value=1.0, v_value=0.0))
-    env.boundary_conditions.append(RightSideFixedVelocityBoundaryCondition(u_value=1.0, v_value=0.0))
-    env.boundary_conditions.append(TopSideFixedVelocityBoundaryCondition(u_value=0.0, v_value=1.0))
-    env.boundary_conditions.append(BottomSideFixedVelocityBoundaryCondition(u_value=0.0, v_value=1.0))
+    env.boundary_conditions.append(
+        LeftSideFixedVelocityBoundaryCondition(u_value=1.0, v_value=0.0)
+    )
+    env.boundary_conditions.append(
+        RightSideFixedVelocityBoundaryCondition(u_value=1.0, v_value=0.0)
+    )
+    env.boundary_conditions.append(
+        TopSideFixedVelocityBoundaryCondition(u_value=0.0, v_value=1.0)
+    )
+    env.boundary_conditions.append(
+        BottomSideFixedVelocityBoundaryCondition(u_value=0.0, v_value=1.0)
+    )
     env.boundary_conditions.append(LeftSidePeriodicBoundaryCondition())
     env.boundary_conditions.append(RightSidePeriodicBoundaryCondition())
 
@@ -46,10 +59,10 @@ def run_simulation():
     # Run the simulation for a specified number of steps
     env.run_many_steps(100)
 
+
 if __name__ == "__main__":
     profiler = cProfile.Profile()
     profiler.enable()
     run_simulation()
     profiler.disable()
-    profiler.dump_stats('simulation_profile.prof')
-
+    profiler.dump_stats("simulation_profile.prof")
